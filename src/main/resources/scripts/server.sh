e93aebd46ecfc754586e7a0a3e48ffb04fb44b1b
Harbor Luo <harborl@users.noreply.github.com>
2016-05-14 08:46:57 +0800

    Server start up script refine

--- src/main/resources/scripts/server.sh
+++ src/main/resources/scripts/server.sh
@@ -3,5 +3,7 @@
 set -h -e
 
 CLASSPATH=.:lib/*
-OPT=-Dconf-file=config/conf-116.properties
-java -server -Xms512M -Xmx1024M -Xss256k -XX:PermSize=256m -XX:MaxPermSize=256m $OPT -cp $CLASSPATH com.homethy.drip.mail.task.server.Bootstrap $@
+java -server \
+     -Xms512M -Xmx1024M -Xss256k -XX:PermSize=256m -XX:MaxPermSize=256m \
+     -cp $CLASSPATH \
+     io.harborl.drip.task.server.Bootstrap $@
